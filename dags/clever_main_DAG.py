cb472c89d07a0a796882ec9355534bf75790d22a
NicholasBaraldi <nicholas.baraldi@gmail.com>
2024-11-05 22:59:52 -0300

    small fixes + gardening

--- dags/clever_main_DAG.py
+++ dags/clever_main_DAG.py
@@ -1,41 +1,45 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
+from scripts.clever_main_pipeline import upload_to_postgres
 
 default_args = {
-    "owner": "alec.ventura",
-    "start_date": datetime(2024, 10, 1),
+    "owner": "nicholas.baraldi",
+    "start_date": datetime(2024, 11, 5),
 }
 
 datasets = [
-    'fmcsa_complaints.csv',
-    'fmcsa_safer_data.csv',
-    'fmcsa_company_snapshot.csv',
-    'fmcsa_companies.csv',
-    'customer_reviews_google.csv',
-    'company_profiles_google_maps.csv'
-
+    "fmcsa_complaints.csv",
+    "fmcsa_safer_data.csv",
+    "fmcsa_company_snapshot.csv",
+    "fmcsa_companies.csv",
+    "customer_reviews_google.csv",
+    "company_profiles_google_maps.csv"
 ]
 
-with DAG("clever_main_DAG", default_args=default_args, catchup=False, schedule_interval='20 0 * * *', max_active_runs=1) as dag:
+with DAG(
+    dag_id="clever_main_DAG",
+    default_args=default_args,
+    catchup=False,
+    schedule_interval="20 0 * * *",
+    max_active_runs=1
+) as dag:
 
-    start_task = EmptyOperator(task_id='Start', dag=dag)
-    finish_task = EmptyOperator(task_id='Finish', dag=dag)
+    start_task = EmptyOperator(task_id="Start", dag=dag)
+    finish_task = EmptyOperator(task_id="Finish", dag=dag)
 
     for file in datasets:
-        file_without_extension = file.split('.')[0]
+        file_without_extension = file.split(".")[0]
 
-        task_id = f"upload_to_postgres_{file_without_extension}"
         upload_to_postgres_task = PythonOperator(
-            task_id=task_id,
+            task_id=f"upload_to_postgres_{file_without_extension}",
             python_callable=upload_to_postgres,
-            dag=dag,
-            execution_timeout=timedelta(milliseconds=2),
             op_kwargs={
-                "file_name": file
-            }
+                "file_name": file,
+                "table_name": file_without_extension
+            },
+            dag=dag
         )
 
-        start_task.set_downstream(upload_to_postgres_task)
-        upload_to_postgres_task.set_downstream(finish_task)
+    start_task >> upload_to_postgres_task >> finish_task
--- dags/scripts/postgres_helper.py
+++ dags/scripts/postgres_helper.py
@@ -4,7 +4,7 @@ import scripts.constants as c
 
 # Create SQLAlchemy engine
 engine = create_engine(
-    f'postgresql+psycopg2://{c.postgres_user}:{c.postgres_password}@{c.postgres_host}:{c.postgres_port}/{c.postgres_dbname}'
+    f"postgresql+psycopg2://{c.postgres_user}:{c.postgres_password}@{c.postgres_host}:{c.postgres_port}/{c.postgres_dbname}"
 )
 
 def run_sql(create_sql):
@@ -15,4 +15,4 @@ def run_sql(create_sql):
 
 def upload_overwrite_table(df, table_name):
     # Upload DataFrame to PostgreSQL
-    df.to_sql(f'{table_name}', engine, index=False, if_exists='replace')
+    df.to_sql(f"{table_name}", engine, index=False, if_exists="replace")
