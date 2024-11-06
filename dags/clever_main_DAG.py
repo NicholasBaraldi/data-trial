30c1b97cba564a44053c64d2612e00392be770e2
NicholasBaraldi <nicholas.baraldi@gmail.com>
2024-11-06 18:44:17 -0300

    add TaskGroup

--- dags/clever_main_DAG.py
+++ dags/clever_main_DAG.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from airflow import DAG
+from airflow.utils.task_group import TaskGroup
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 from scripts.clever_main_pipeline import upload_to_postgres
@@ -27,19 +28,21 @@ with DAG(
 ) as dag:
 
     start_task = EmptyOperator(task_id="Start", dag=dag)
-    finish_task = EmptyOperator(task_id="Finish", dag=dag)
+    finish_task = EmptyOperator(task_id="Finish", trigger_rule="none_failed", dag=dag)
 
-    for file in datasets:
-        file_without_extension = file.split(".")[0]
+    with TaskGroup("datasets") as group:
 
-        upload_to_postgres_task = PythonOperator(
-            task_id=f"upload_to_postgres_{file_without_extension}",
-            python_callable=upload_to_postgres,
-            op_kwargs={
-                "file_name": file,
-                "table_name": file_without_extension
-            },
-            dag=dag
-        )
+        for file in datasets:
+            file_without_extension = file.split(".")[0]
 
-    start_task >> upload_to_postgres_task >> finish_task
+            upload_to_postgres_task = PythonOperator(
+                task_id=f"upload_to_postgres_{file_without_extension}",
+                python_callable=upload_to_postgres,
+                op_kwargs={
+                    "file_name": file,
+                    "table_name": file_without_extension
+                },
+                dag=dag
+            )
+
+    start_task >> group >> finish_task
