1bf342ee6aa0bc621bf4df99c47a4f3b7b012c63
NicholasBaraldi <nicholas.baraldi@gmail.com>
2024-11-10 23:56:01 -0300

    adding logs and docstring

--- dags/scripts/clever_main_pipeline.py
+++ dags/scripts/clever_main_pipeline.py
@@ -1,8 +1,18 @@
+import logging
 import pandas as pd
 from scripts.postgres_helper import upload_overwrite_table, run_sql
 
+LOGGER = logging.getLogger("airflow.task")
 
 def upload_to_postgres(**kwargs):
+    """
+    Uploads a CSV file to a PostgreSQL database, overwriting any existing data in the specified table.
+
+    Parameters:
+        kwargs (dict):
+            - file_name (str): The name of the CSV file to upload.
+            - table_name (str): The name of the target table in the PostgreSQL database.
+    """
     file_name=kwargs.get("file_name")
     table_name = kwargs.get("table_name")
 
@@ -10,9 +20,18 @@ def upload_to_postgres(**kwargs):
         f"dags/scripts/data_examples/{file_name}", escapechar="\\"
     )
 
+    LOGGER.info(f"uploading {table_name} to DB")
+
     upload_overwrite_table(raw_df, table_name)
 
 def transform_data(**kwargs):
+    """
+    Executes a SQL query from a file to transform data in the PostgreSQL database.
+
+    Parameters:
+        kwargs (dict):
+            - query_file (str): The filename of the SQL query to execute.
+    """
     query_file = kwargs.get("query_file")
 
     query_path = f"queries/{query_file}"
--- dags/scripts/postgres_helper.py
+++ dags/scripts/postgres_helper.py
@@ -1,17 +1,37 @@
+import logging
+import pandas as pandas
 from sqlalchemy import create_engine, text
 import scripts.constants as c
+
+LOGGER = logging.getLogger("airflow.task")
 
 # Create SQLAlchemy engine
 engine = create_engine(
     f"postgresql+psycopg2://{c.postgres_user}:{c.postgres_password}@{c.postgres_host}:{c.postgres_port}/{c.postgres_dbname}"
 )
 
-def run_sql(path):
+def run_sql(path: str) -> None:
+    """
+    Executes an SQL query stored in a specified file.
+
+    Parameters:
+        path (str): The file path of the SQL query to execute.
+    """
     with open(path) as f:
         with engine.connect() as conn:
+            LOGGER.info(f"Executing query: {f.read()}")
             conn.execute(text(f.read()))
             conn.close()
 
-def upload_overwrite_table(df, table_name):
-    # Upload DataFrame to PostgreSQL
+def upload_overwrite_table(df: pandas.DataFrame, table_name: str) -> None:
+    """
+    Uploads a pandas DataFrame to a specified table in a PostgreSQL database, overwriting the existing data.
+
+    Parameters:
+        df (pandas.DataFrame): The DataFrame containing data to upload.
+        table_name (str): The name of the target table in the PostgreSQL database.
+
+    Notes:
+        If the specified table exists, it will be replaced by the DataFrame data.
+    """
     df.to_sql(f"{table_name}", engine, index=False, if_exists="replace")
